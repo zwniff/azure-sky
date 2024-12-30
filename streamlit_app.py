2f4c838a0924d897d20bee0e24debd9d3694c23b
zwniff <willianz@pm.me>
2024-12-30 18:28:45 +0700

    Add example data buttons in Streamlit app for user-friendly input selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,26 @@ import pickle as pkl
 import pandas as pd
 from wrapper_model import XGBWrapper, CTBWrapper
 
+# load example data
+example_data = pd.read_csv('./data/example_data.csv')
+
 # Load the model, pca and scaler
 with open('./models/stacking_model.pkl', 'rb') as file:
     stacking_model = pkl.load(file)
-
 with open('./models/pca.pkl', 'rb') as file:
     pca = pkl.load(file)
-
 with open('./models/scaler.pkl', 'rb') as file:
     scaler = pkl.load(file)
 
 # load base model
 with open('./models/ctb_model.pkl', 'rb') as file:
     ctb_model = pkl.load(file)
-
 with open('./models/lgbm_model.pkl', 'rb') as file:
     lgbm = pkl.load(file)
-
 with open('./models/xgb_model.pkl', 'rb') as file:
     xgb_model = pkl.load(file)
-
 with open('./models/dc_model.pkl', 'rb') as file:
     dc = pkl.load(file)
-
 with open('./models/svm_model.pkl', 'rb') as file:
     svm = pkl.load(file)
 
@@ -33,7 +30,40 @@ with open('./models/svm_model.pkl', 'rb') as file:
 st.title('Predicting the AQI Category')
 
 st.write('This app predicts the AQI category based on the input features.')
+st.write('Please enter the input features in the sidebar to get the prediction or use these example data.')
+for i in range(len(example_data)):
+    st.button(f'Example {i+1}')
+    # Add buttons for example data
+    if st.button('Use Example 1'):
+        example = example_data.iloc[0]
+    elif st.button('Use Example 2'):
+        example = example_data.iloc[1]
+    elif st.button('Use Example 3'):
+        example = example_data.iloc[2]
+    elif st.button('Use Example 4'):
+        example = example_data.iloc[3]
+    elif st.button('Use Example 5'):
+        example = example_data.iloc[4]
+    else:
+        example = None
 
+    # If an example is selected, update the input fields
+    if example is not None:
+        pollutant_pm25 = st.sidebar.number_input('Pollutant_PM2.5_µg/m³', min_value=0.0, max_value=500.0, value=example['Pollutant_PM2.5_µg/m³'])
+        pollutant_pm10 = st.sidebar.number_input('Pollutant_PM10_µg/m³', min_value=0.0, max_value=500.0, value=example['Pollutant_PM10_µg/m³'])
+        pollutant_o3 = st.sidebar.number_input('Pollutant_O3_ppb', min_value=0.0, max_value=500.0, value=example['Pollutant_O3_ppb'])
+        pollutant_no2 = st.sidebar.number_input('Pollutant_NO2_ppb', min_value=0.0, max_value=500.0, value=example['Pollutant_NO2_ppb'])
+        pollutant_co = st.sidebar.number_input('Pollutant_CO_ppm', min_value=0.0, max_value=500.0, value=example['Pollutant_CO_ppm'])
+        pollutant_so2 = st.sidebar.number_input('Pollutant_SO2_ppb', min_value=0.0, max_value=500.0, value=example['Pollutant_SO2_ppb'])
+        urban_vegetation_area = st.sidebar.number_input('UrbanVegetationArea_m2', min_value=0.0, max_value=500.0, value=example['UrbanVegetationArea_m2'])
+        humidity = st.sidebar.number_input('Humidity_%', min_value=0.0, max_value=100.0, value=example['Humidity_%'])
+        air_temperature = st.sidebar.number_input('AirTemperature_C', min_value=-50.0, max_value=50.0, value=example['AirTemperature_C'])
+        annual_energy_savings = st.sidebar.number_input('AnnualEnergySavings_%', min_value=0.0, max_value=100.0, value=example['AnnualEnergySavings_%'])
+        population_density = st.sidebar.number_input('PopulationDensity_people/km²', min_value=0.0, max_value=100000.0, value=example['PopulationDensity_people/km²'])
+        renewable_energy_percentage = st.sidebar.number_input('RenewableEnergyPercentage_%', min_value=0.0, max_value=100.0, value=example['RenewableEnergyPercentage_%'])
+        annual_energy_consumption = st.sidebar.number_input('AnnualEnergyConsumption_kWh', min_value=0.0, max_value=1000000.0, value=example['AnnualEnergyConsumption_kWh'])
+        green_space_index = st.sidebar.number_input('GreenSpaceIndex_%', min_value=0.0, max_value=100.0, value=example['GreenSpaceIndex_%'])
+        historic_pollutant_levels = st.sidebar.number_input('HistoricPollutantLevels', min_value=0.0, max_value=100.0, value=example['HistoricPollutantLevels'])
 # Input features
 st.sidebar.title('Input Features')
 
@@ -89,25 +119,3 @@ st.markdown(f'The predicted AQI category is: <span style="color:{color_codes[pre
 st.title('Prediction Probability')
 st.write('The probability of each class is:')
 st.write(probability)
-
-example = pd.read_csv('./data/example_data.csv')
-
-# Display the example data with clickable buttons to fill the input features for each category example
-st.sidebar.title('Example Data')
-for i in range(len(example)):
-    if st.sidebar.button(f'Fill with example data {i+1}'):
-        st.session_state['Pollutant_PM2.5_µg/m³'] = example.iloc[i]['Pollutant_PM2.5_µg/m³']
-        st.session_state['Pollutant_PM10_µg/m³'] = example.iloc[i]['Pollutant_PM10_µg/m³']
-        st.session_state['Pollutant_O3_ppb'] = example.iloc[i]['Pollutant_O3_ppb']
-        st.session_state['Pollutant_NO2_ppb'] = example.iloc[i]['Pollutant_NO2_ppb']
-        st.session_state['Pollutant_CO_ppm'] = example.iloc[i]['Pollutant_CO_ppm']
-        st.session_state['Pollutant_SO2_ppb'] = example.iloc[i]['Pollutant_SO2_ppb']
-        st.session_state['UrbanVegetationArea_m2'] = example.iloc[i]['UrbanVegetationArea_m2']
-        st.session_state['Humidity_%'] = example.iloc[i]['Humidity_%']
-        st.session_state['AirTemperature_C'] = example.iloc[i]['AirTemperature_C']
-        st.session_state['AnnualEnergySavings_%'] = example.iloc[i]['AnnualEnergySavings_%']
-        st.session_state['PopulationDensity_people/km²'] = example.iloc[i]['PopulationDensity_people/km²']
-        st.session_state['RenewableEnergyPercentage_%'] = example.iloc[i]['RenewableEnergyPercentage_%']
-        st.session_state['AnnualEnergyConsumption_kWh'] = example.iloc[i]['AnnualEnergyConsumption_kWh']
-        st.session_state['GreenSpaceIndex_%'] = example.iloc[i]['GreenSpaceIndex_%']
-        st.session_state['HistoricPollutantLevels'] = example.iloc[i]['HistoricPollutantLevels']
